127b46038015d2494da1dd564b2a3fce65753e58
Gao-Wei Chang <128176822+GaoWeiChang@users.noreply.github.com>
2025-10-12 19:40:54 +0800

    add Notification

---
+++ book_store/Views/Shared/_Notification.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace book_store.Views.Shared
+{
+    public class _NotificationModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
